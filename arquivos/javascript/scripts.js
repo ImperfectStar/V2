ec87a11abc7087cef317a03bd1f28a0d7bb16ad4
ImperfectStar <151389220+ImperfectStar@users.noreply.github.com>
2025-01-21 11:22:17 -0300

    Limpeza de codigo

---
+++ arquivos/javascript/scripts.js
@@ -0,0 +1,19 @@
+
+function showhide(nomeItem){
+    var itens = document.getElementById(nomeItem)
+    if (itens.style.display == 'block'){
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+    }
+}
+function mudar(x) {
+    x.classList.toggle("change");
+}
+function sidemenu(smenu){
+    if (smenu.style.display == 'flex') {
+        smenu.style.display = 'none'
+    } else {
+        smenu.style.display = 'flex'
+    }
+}
